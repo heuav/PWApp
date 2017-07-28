a33e37022795afeb23beb3235a9e640cf0c20c47
heuav <taylorwboyer@gmail.com>
2017-07-28 15:25:06 -0400

    First Commit

---
+++ Assets/Control1.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Control1 : MonoBehaviour
+{
+	public void NextScene()
+	{
+		SceneManager.LoadScene("scimoresAcademy");
+	}
+}
